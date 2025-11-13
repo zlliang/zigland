cf6f686fb1a15713353c90cfd78e88802debe6d3
Zilong Liang <zlliang96@outlook.com>
2025-11-13 16:07:23 +0800

    feat: reuse existing sandboxes when running codes from playground

--- src/api/routes/run.ts
+++ src/api/routes/run.ts
@@ -1,90 +1,132 @@
 import { z } from "zod"
 import { Sandbox } from "@vercel/sandbox"
+import { nanoid } from "nanoid"
 
 export const schema = z.object({
   code: z.string().min(1, "Code is required"),
 })
 
 export async function* handler({ input }: { input: z.infer<typeof schema> }) {
+  let sandbox: Sandbox
+  let reusing = false
+  const zigDir = "compiler"
+  const zigFileName = `${nanoid(6)}.zig`
+
   // Initialize sandbox
 
-  yield "==> Initializing running sandbox...\n"
-
-  const sandbox = await Sandbox.create({
+  const list = await Sandbox.list({
     teamId: import.meta.env.VERCEL_TEAM_ID,
     projectId: import.meta.env.VERCEL_PROJECT_ID,
     token: import.meta.env.VERCEL_TOKEN,
   })
 
+  const runningSandboxes = list.json.sandboxes.filter(sandbox => sandbox.status === "running")
+  if (runningSandboxes.length > 0) {
+    yield "==> Using existing running sandbox...\n"
+    sandbox = await Sandbox.get({
+      teamId: import.meta.env.VERCEL_TEAM_ID,
+      projectId: import.meta.env.VERCEL_PROJECT_ID,
+      token: import.meta.env.VERCEL_TOKEN,
+      sandboxId: runningSandboxes[0].id,
+    })
+
+    reusing = true
+  } else {
+    yield "==> No running sandboxes found, creating a new one...\n"
+
+    sandbox = await Sandbox.create({
+      teamId: import.meta.env.VERCEL_TEAM_ID,
+      projectId: import.meta.env.VERCEL_PROJECT_ID,
+      token: import.meta.env.VERCEL_TOKEN,
+      timeout: 20 * 60 * 1000, // 20 minutes
+    })
+  }
+
   // Write files
 
   await sandbox.writeFiles([
-    { path: "./source/main.zig", content: Buffer.from(input.code) },
+    { path: `./source/${zigFileName}`, content: Buffer.from(input.code) },
   ])
 
-  // Install system dependencies
+  if (!reusing) {
+    // Install system dependencies
 
-  yield "==> Installing system dependencies...\n"
+    yield "==> Installing system dependencies...\n"
 
-  let systemPrepareCmd = await sandbox.runCommand({
-    cmd: "dnf",
-    args: ["-q", "install", "-y", "xz", "wget"],
-    sudo: true,
-    detached: true,
-  })
+    let systemPrepareCmd = await sandbox.runCommand({
+      cmd: "dnf",
+      args: ["-q", "install", "-y", "xz", "wget"],
+      sudo: true,
+      detached: true,
+    })
 
-  for await (const log of systemPrepareCmd.logs()) {
-    yield log.data
-  }
+    for await (const log of systemPrepareCmd.logs()) {
+      yield log.data
+    }
 
-  const systemPrepareResult = await systemPrepareCmd.wait()
-  if (systemPrepareResult.exitCode !== 0) {
-    return
-  }
+    const systemPrepareResult = await systemPrepareCmd.wait()
+    if (systemPrepareResult.exitCode !== 0) {
+      return
+    }
 
-  // Get Zig tarball URL
+    // Get Zig tarball URL
 
-  yield "==> Downloading Zig compiler...\n"
+    yield "==> Downloading Zig compiler...\n"
 
-  const zigManifest = await fetch("https://ziglang.org/download/index.json").then(res => res.json())
-  const zigTarballLink = zigManifest["master"]["x86_64-linux"]["tarball"]
-  const zigTarballName = zigTarballLink.split("/").pop()
-  const zigDir = zigTarballName.split(".tar.xz")[0]
+    const zigManifest = await fetch("https://ziglang.org/download/index.json").then(res => res.json())
+    const zigTarballLink = zigManifest["master"]["x86_64-linux"]["tarball"] as string
+    const zigTarballName = zigTarballLink.split("/").pop() as string
 
-  // Download the tarball
+    // Download the tarball
 
-  const downloadCmd = await sandbox.runCommand({
-    cmd: "wget",
-    args: ["--no-verbose", zigTarballLink],
-    detached: true,
-  })
+    const downloadCmd = await sandbox.runCommand({
+      cmd: "wget",
+      args: ["--no-verbose", zigTarballLink],
+      detached: true,
+    })
 
-  for await (const log of downloadCmd.logs()) {
-    yield log.data
-  }
+    for await (const log of downloadCmd.logs()) {
+      yield log.data
+    }
 
-  const downloadResult = await downloadCmd.wait()
-  if (downloadResult.exitCode !== 0) {
-    return
-  }
+    const downloadResult = await downloadCmd.wait()
+    if (downloadResult.exitCode !== 0) {
+      return
+    }
 
-  // Extract the executable to ./compiler
+    // Extract the executable to ./compiler
 
-  yield "==> Extracting Zig compiler...\n"
+    yield "==> Extracting Zig compiler...\n"
 
-  const extractCmd = await sandbox.runCommand({
-    cmd: "tar",
-    args: ["-xf", zigTarballName],
-    detached: true,
-  })
+    const extractCmd = await sandbox.runCommand({
+      cmd: "tar",
+      args: ["-xf", zigTarballName],
+      detached: true,
+    })
 
-  for await (const log of extractCmd.logs()) {
-    yield log.data
-  }
+    for await (const log of extractCmd.logs()) {
+      yield log.data
+    }
 
-  const extractResult = await extractCmd.wait()
-  if (extractResult.exitCode !== 0) {
-    return
+    const extractResult = await extractCmd.wait()
+    if (extractResult.exitCode !== 0) {
+      return
+    }
+
+    const renameCmd = await sandbox.runCommand({
+      cmd: "mv",
+      args: [zigTarballName.split(".tar.xz")[0], zigDir],
+      detached: true,
+    })
+
+    for await (const log of renameCmd.logs()) {
+      yield log.data
+    }
+
+    const renameResult = await renameCmd.wait()
+    if (renameResult.exitCode !== 0) {
+      return
+    }
   }
 
   // Output the Zig version
@@ -112,7 +154,7 @@ export async function* handler({ input }: { input: z.infer<typeof schema> }) {
 
   const runCmd = await sandbox.runCommand({
     cmd: `./${zigDir}/zig`,
-    args: ["run", "./source/main.zig"],
+    args: ["run", `./source/${zigFileName}`],
     detached: true,
   })
 
